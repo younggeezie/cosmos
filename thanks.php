374230e4025066e75e6e9a377fc066a70467c0e7
younggeezie <63011162+younggeezie@users.noreply.github.com>
2020-04-01 19:27:00 -0700

    Add files via upload

---
+++ thanks.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cosmos Chi Kung Society - Thanks</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,800" rel="stylesheet">
+</head>
+
+<body>
+	<ul class="navbar">
+		<li><a href="index.html">Home</a></li>
+		<li class="dropdown">
+		<a href="javascript:void(0)" class="dropbtn">Disciplines</a>
+    <div class="dropdown-content">
+      <a href="yang.html">Yang Style Tai Chi</a>
+      <a href="qigong.html">Qi Gong</a>
+      <a href="lde.html">Long Distance Energy</a>
+			<a href="fire.html">Firewalking</a>
+    </div>
+		<li><a href="schedule.html">Classes</a></li>
+		<li><a class="active" href="contact.html">Contact</a></li>
+		<li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">About</a>
+    <div class="dropdown-content">
+      <a href="about.html">Sifu Lawrence Der</a>
+      <a href="location.html">Location</a>
+      <a href="resources.html">Resources</a>
+    </div>
+	</ul>
+<div id="top">
+		<h1>Cosmos Chi Kung Society</h1>
+</div>
+<div id="contentleft">
+		<h3>Thanks a lot, <?=$_POST["name"]; ?>!</h3>
+		<p>Lawrence will get back to you soon. He thanks you for your interest in Cosmos Chi Kung Society!</p>
+</div>
+<div id="sitemap">
+	<a href="sitemap.html">Sitemap</a>
+</div>
+
+</body>
+
+</html>
